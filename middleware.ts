1f9198b7d52cf3799ae0032dd807281cf853e476
peuhaba-dev <peuhabashop@gmail.com>
2026-04-19 00:05:22 +0700

    cookies coba-coba

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,13 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(
+  cookiesToSet: {
+    name: string
+    value: string
+    options?: Record<string, any>
+  }[]
+) {
           cookiesToSet.forEach(({ name, value, options }) => {
             response.cookies.set(name, value, options)
           })
